9e90ed827de80aee25b16f1bdfe655425b4c40ce
Alain CAO <alaincao17@gmail.com>
2011-01-27 23:24:30 +0100

    Backup commit

--- Resources/Resources.cs
+++ Resources/Resources.cs
@@ -6,7 +6,7 @@ using System.Windows.Media.Imaging;
 
 namespace CommonLibs.Resources
 {
-	public static class Resources
+	public static class Common
 	{
 		public static BitmapImage		ImagePause;
 		public static BitmapImage		ImagePlay;
@@ -14,11 +14,11 @@ namespace CommonLibs.Resources
 		public static BitmapImage		ImageInfo;
 		public static BitmapImage		ImageError;
 
-		static Resources()
+		static Common()
 		{
 			System.Diagnostics.Debug.Assert( UriParser.IsKnownScheme("pack"), "This method must be called after the initialization of any WPF UserControls to initialize URI stuffs..." );
 
-			var assemblyName = typeof(Resources).Assembly.FullName;
+			var assemblyName = typeof(Common).Assembly.FullName;
 			string uriPrefix = "pack://application:,,,/" + assemblyName + ";component/Resources/";
 			ImagePause =		new BitmapImage( new Uri(uriPrefix + "127166-simple-black-square-icon-media-a-media27-pause-sign.png") );
 			ImagePlay =			new BitmapImage( new Uri(uriPrefix + "126552-simple-black-square-icon-arrows-triangle-solid-right.png") );
@@ -26,5 +26,21 @@ namespace CommonLibs.Resources
 			ImageInfo =			new BitmapImage( new Uri(uriPrefix + "070241-firey-orange-jelly-icon-alphanumeric-information2-ps.png") );
 			ImageError =		new BitmapImage( new Uri(uriPrefix + "074367-simple-red-glossy-icon-alphanumeric-circled-x.png") );
 		}
+	}
+
+	public static class WPF_SetupGui_Actions
+	{
+		public static BitmapImage		ImagePending		{ get { return Common.ImagePause; } }
+		public static BitmapImage		ImageRunning		{ get { return Common.ImagePlay; } }
+		public static BitmapImage		ImageError			{ get { return Common.ImageError; } }
+		public static BitmapImage		ImageWarnings		{ get { return Common.ImageInfo; } }
+		public static BitmapImage		ImageSuccess		{ get { return Common.ImageCheckMark; } }
+	}
+
+	public static class WPF_SetupGui_UserControls_IISWebApplication
+	{
+		public static BitmapImage		ImageRunning		{ get { return Common.ImagePlay; } }
+		public static BitmapImage		ImageSuccess		{ get { return Common.ImageCheckMark; } }
+		public static BitmapImage		ImageError			{ get { return Common.ImageError; } }
 	}
 }
