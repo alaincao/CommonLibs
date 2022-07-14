c2d74d9083cbd1842f7790dc7a680803dc154dee
Alain CAO <acao@prophix.com>
2022-07-14 18:06:17 +0200

    Updated license to Zero-Clause BSD cf. https://opensource.org/licenses/0BSD

--- Web/LongPolling/WebSocketHandler.cs
+++ Web/LongPolling/WebSocketHandler.cs
@@ -1,4 +1,24 @@
-﻿using System;
+﻿//
+// CommonLibs/Web/LongPolling/WebSocketHandler.cs
+//
+// Author:
+//   Alain CAO (alain.cao@sigmaconso.com)
+//
+// Copyright (c) 2020 - 2021 Alain CAO
+//
+// Permission to use, copy, modify, and/or distribute this software for any
+// purpose with or without fee is hereby granted.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+// INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+//
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
